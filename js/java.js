0cc2c860ae06fdf0073e2c91fdcd3ec66be915e7
Harris Junaid <harris.junaid@gmail.com>
2023-01-29 05:46:18 -0500

    a commit

---
+++ js/java.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
